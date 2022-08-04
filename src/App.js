a52a4279e20ff618a389874eb1a9c1c7a0a0f615
Dolsen0 <derek.olsen@gmail.com>
2022-08-04 14:08:31 -0400

    added count for button

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
+import { useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let count = 0;
-  return (
+ // let count = 0;
+ const [count,setCount] = useState(0) 
+ return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>My First React App</h1>
+        <h5>now on ipad</h5>
         <p>
           You clicked the button {count} times.
         </p>
         <button onClick={() =>{
-          console.log(`I clicked the button ${count} times`)
-          count = count +1;
-        }}>Click Me</button>
+          console.log(`I clicked the button ${count + 1} times`)
+
+          //count = count +1;
+          setCount(count + 1)
+        }}>Add one</button>
    
       </header>
     </div>
